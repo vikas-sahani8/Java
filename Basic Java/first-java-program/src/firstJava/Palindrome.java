b1646efbccb567be6dfcac111ebaca71adb31000
vikas-sahani8 <sahani8vikas@gmail.com>
2025-02-27 20:20:50 +0530

    Palindrome

--- Basic Java/first-java-program/src/firstJava/Palindrome.java
+++ Basic Java/first-java-program/src/firstJava/Palindrome.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = scanner.next();
+        scanner.close();
 
-        String str = scn.nextLine();
-        StringBuilder rvs = new StringBuilder();
-        for(int i = str.length()-1; i>=0;i--){
-            rvs.append(str.charAt(i));
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
         }
-        if (str.contentEquals(rvs))
-            System.out.println(str+" is a palindrome");
-        else
-            System.out.println(str+" is not a palindrome");
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
     }
 }
